8ec3424d801072db06e70e485de4d1f5ed31a3fb
Yindeish <adeshinaadam03@gmail.com>
2025-04-26 18:45:45 +0100

    refactor: enhance ticket context and grid view for improved state management and display

--- components/tickets/pageComponents.tsx
+++ components/tickets/pageComponents.tsx
@@ -6,35 +6,32 @@ import { Expand } from "@/public/icons/homeSvgs";
 import { useState } from "react";
 import { useTicketContext } from "@/context.state/ticket";
 import TicketModal from "./modalComponents";
+import { Utils } from "@/utils";
 
-function TicketGridView({ currentFilter }: { currentFilter: "user" | "driver" }) {
+function TicketGridView() {
     const { showModal } = useModal()
-    const { handlers } = useTicketContext()
-
-    const arr = Array.from({ length: 20 });
+    const { state, handlers } = useTicketContext()
 
     return (<div className="w-[85%] h-[calc(100%-74px)] mx-auto py-[1em] bg-f9f7f8 overflow-y-scroll">
 
         <div className="w-full h-fit grid grid-cols-3 gap-[1em]">
-            {arr.map((_, index) => (
+            {state.local.displayTickets?.map((ticket, index) => (
                 <div
                     className="col-span-1 w-full h-[40vh] flex flex-col gap-[1em] rounded-[20px] p-[1.3em] border-[0.7px] border-FFAE02 bg-white" style={{ boxShadow: '0px 0px 10px 0px #00000010' }} key={index}>
 
                     <div className="w-full h-fit flex items-start justify-between">
 
                         <div className="w-[70%] flex flex-col gap-[12px]">
-                            <span className="font-medium text-[14px] text-black">Ride #1234567XYZ</span>
+                            <span className="font-medium text-[14px] text-black">Ticket #{ticket?._id?.slice(0, 10)}</span>
 
                             <span className="inline-flex gap-[0.3em] items-center w-fit h-fit">
                                 <LuClock3 className="text-27AE65 w-[16px] h-[16px]" />
-                                <span className="font-normal text-[12px] text-747474">15:23</span></span>
+                                <span className="font-normal text-[12px] text-747474">{Utils.formatTime(ticket?.createdAt)}</span></span>
                         </div>
 
                         <Expand onClick={() => {
                             handlers.setLocalState({
-                                key: 'selectedTicket', value: {
-
-                                }
+                                key: 'selectedTicket', value: ticket
                             })
                             showModal(<TicketModal />, false)
                         }} className="w-[18px] h-[18px] text-747474 cursor-pointer" />
@@ -42,7 +39,7 @@ function TicketGridView({ currentFilter }: { currentFilter: "user" | "driver" })
 
                     <KeyValueBlock
                         keyValueArray={
-                            currentFilter === 'driver' ?
+                            state.inputs.method === 'driver' ?
                                 [
                                     { key: 'Driver Name', value: 'Folagbade Roman' },
                                     { key: 'Phone Number', value: '+234 715 615 8715' },
--- context.state/ticket.tsx
+++ context.state/ticket.tsx
@@ -1,15 +1,19 @@
 "use client";
 import { createContext, FC, ReactNode, useContext, useState } from "react";
+import { IPlan } from "./route";
 
 // !Interfaces
 // !Input State
-interface ITicketContextInputState {}
+interface ITicketContextInputState {
+  status: "pending" | "resolved";
+  method: "user" | "driver";
+}
 // !Input State
 
 // !Local State
 interface ITicketContextLocalState {
-  allTickets: [];
-  newTickets: [];
+  allTickets: ITicket[];
+  displayTickets: ITicket[];
   selectedTicket: ITicket | null;
 }
 // !Local State
@@ -27,7 +31,47 @@ interface ITicketContextState {
 }
 
 // !Individuals
-export interface ITicket {}
+export type TPaymentStatus = "idle" | "successful" | "pending" | "failed";
+export type TPaymentOption = "wallet" | "cash" | "point" | "bank-transfer";
+export type TTicketSatus = "idle" | "cancelled" | "paid" | "booked";
+
+interface ITicketPayment {
+  paymentStatus: TPaymentStatus;
+  paymentOption: TPaymentOption;
+  riderCounterOffer: number;
+  ride: {
+    rideFee: number;
+    ridePlanFee: number;
+  };
+  trip: {
+    tripFee: number;
+    tripPlanFee: number;
+  };
+}
+
+export interface ITicket {
+  _id: string;
+  riderId: string;
+  riderCounterOffer: number;
+  quantity: number;
+  ticketOtp: number;
+  ticketStatus: TTicketSatus;
+  payment: ITicketPayment;
+  ride: {
+    currentRideId: string;
+    riderRideDetailsId: string;
+    ridePlan: IPlan;
+    rideFee: number;
+  };
+  trip: {
+    currentTripId: string;
+    riderTripDetailsId: string;
+    tripPlan: IPlan;
+    tripFee: number;
+  };
+  createdAt: string;
+  updatedAt: string;
+}
 // !Individuals
 
 interface ITicketContext {
@@ -64,61 +108,64 @@ function TicketContextProvider({ children }: { children: ReactNode }) {
     fetch: {
       fetchingTickets: false,
     },
-    inputs: {},
+    inputs: {
+      status: "pending",
+      method: "user",
+    },
     local: {
       allTickets: [],
-      newTickets: [],
+      displayTickets: [],
       selectedTicket: null,
     },
   });
 
   const setFetchState = ({
-      key,
-      value,
-    }: {
-      key: keyof ITicketContextFetchState;
-      value: any;
-    }) => {
-        updateState((prev) => ({
-            ...prev,
-            fetch: {
-                ...prev.fetch,
-                [key]: value
-            }
-        }))
-    };
+    key,
+    value,
+  }: {
+    key: keyof ITicketContextFetchState;
+    value: any;
+  }) => {
+    updateState((prev) => ({
+      ...prev,
+      fetch: {
+        ...prev.fetch,
+        [key]: value,
+      },
+    }));
+  };
 
   const setInputState = ({
-      key,
-      value,
-    }: {
-      key: keyof ITicketContextInputState;
-      value: any;
-    }) => {
-        updateState((prev) => ({
-            ...prev,
-            inputs: {
-                ...prev.inputs,
-                [key]: value
-            }
-        }))
-    };
+    key,
+    value,
+  }: {
+    key: keyof ITicketContextInputState;
+    value: any;
+  }) => {
+    updateState((prev) => ({
+      ...prev,
+      inputs: {
+        ...prev.inputs,
+        [key]: value,
+      },
+    }));
+  };
 
   const setLocalState = ({
-      key,
-      value,
-    }: {
-      key: keyof ITicketContextLocalState;
-      value: any;
-    }) => {
-        updateState((prev) => ({
-            ...prev,
-            local: {
-                ...prev.local,
-                [key]: value
-            }
-        }))
-    };
+    key,
+    value,
+  }: {
+    key: keyof ITicketContextLocalState;
+    value: any;
+  }) => {
+    updateState((prev) => ({
+      ...prev,
+      local: {
+        ...prev.local,
+        [key]: value,
+      },
+    }));
+  };
 
   return (
     <TicketContext.Provider
