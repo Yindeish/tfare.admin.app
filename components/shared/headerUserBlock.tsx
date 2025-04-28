c104a897d6944e91c18394e5715cf9fcab8b042a
Yindeish <adeshinaadam03@gmail.com>
2025-04-28 18:19:09 +0100

    feat: add navigation and dropdown menu components, enhance user block with menu integration

--- components/shared/headerUserBlock.tsx
+++ components/shared/headerUserBlock.tsx
@@ -1,24 +1,56 @@
 import shared_images from "@/constants/images/shared";
 import { useAuthContext } from "@/context.state/auth";
 import { IoChevronDownSharp } from "react-icons/io5";
-
-
+import MenuTile from "./menuTile";
 
 function HeaderUserBlock() {
-    const {state: {local: {user}}} = useAuthContext()
+  const {
+    state: {
+      local: { user },
+    },
+    handlers: { signout },
+  } = useAuthContext();
 
-    return (
-        <div className="flex items-center gap-[1em] w-fit h-fit cursor-pointer">
-            <img className="w-[50px] h-[50px] rounded-full" src={user?.picture} alt="" />
+  return (
+    <div className="w-fit h-fit">
+      <MenuTile
+        triggerProps={{
+          children: (
+            <div className="flex items-center gap-[1em] w-fit h-fit cursor-pointer">
+              <img
+                className="w-[50px] h-[50px] rounded-full"
+                src={user?.picture}
+                alt=""
+              />
 
-            <div className="flex flex-col gap-[0.3em] ">
-                <span className="font-bold text-[16px] text-black">{user?.fullName}</span>
-                <span className="font-normal text-[16px] text-747474">{user?.email}</span>
+              <div className="flex flex-col items-start gap-[0.3em] ">
+                <span className="font-bold text-[16px] text-black">
+                  {user?.fullName}
+                </span>
+                <span className="font-normal text-[16px] text-747474">
+                  {user?.email}
+                </span>
+              </div>
+
+              <IoChevronDownSharp className="text-747474 w-[30px] h-[20px]" />
             </div>
-
-            <IoChevronDownSharp className="text-747474 w-[30px] h-[20px]" />
-        </div>
-    )
+          ),
+        }}
+        list={[
+          {
+            children: (
+              <span className="text-[16px] font-normal text-CF0707 pt-[1em] cursor-pointer">
+                Logout
+              </span>
+            ),
+            onClick: () => {
+              signout();
+            },
+        }
+        ]}
+      />
+    </div>
+  );
 }
 
 export default HeaderUserBlock;
